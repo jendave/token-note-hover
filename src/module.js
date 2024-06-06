80da743dc88e84d0ee0ea18cf776cea3db7a419c
David Hudson <david.hudson@disney.com>
2024-06-06 02:08:52 -0700

    more cleanups

--- src/module.js
+++ src/module.js
@@ -5,10 +5,6 @@ import CONSTANTS from './scripts/constants';
 import registerSettings from './scripts/settings';
 import TokenNoteHoverHUD from './scripts/TokenNoteHoverHUD';
 
-// const API = {
-//   tokenNoteHover: new TokenNoteHover(),
-// };
-
 /* -------------------------------------------------------------------------- */
 /*                                    Hooks                                   */
 /* -------------------------------------------------------------------------- */
@@ -37,40 +33,20 @@ Hooks.on('renderHeadsUpDisplay', (app, html, data) => {
  */
 Hooks.on('hoverToken', (note, hovered) => {
   if (game.settings.get(CONSTANTS.MODULE_ID, 'hoverEnabled')) {
-    let tooltipForceRemoveS = String(
-      foundry.utils.getProperty(note, `document.flags.${CONSTANTS.MODULE_ID}.${CONSTANTS.FLAGS.TOOLTIP_FORCE_REMOVE}`),
-    );
-    if (tooltipForceRemoveS !== 'true' && tooltipForceRemoveS !== 'false') {
-      tooltipForceRemoveS = 'false';
-    }
-    const tooltipForceRemove = String(tooltipForceRemoveS) === 'true';
-
-    if (!hovered) {
-      clearTimeout(this.hoverTimer);
-      if (tooltipForceRemove) {
-        $('#powerTip').remove();
-      }
-      return canvas.hud.tokenNoteHover.clear();
-    }
-
     const ownershipPermissionsRequired = game.settings.get(CONSTANTS.MODULE_ID, 'ownershipPermissionsRequired');
     const tooltipDelay = game.settings.get(CONSTANTS.MODULE_ID, 'tooltipDelay');
 
-    // If the note is hovered by the mouse cursor (not via alt/option)
     if (hovered) {
-      this.hoverTimer = setTimeout(() => {
+      setTimeout(() => {
         if (note.interactionState === 1
           && (note.actor.permission >= ownershipPermissionsRequired
             || note.actor.ownership.default === -1)) {
           canvas.hud.tokenNoteHover.bind(note);
         }
       }, tooltipDelay);
-    } else if (!hovered) {
-      // This code should be never reached
-      clearTimeout(this.hoverTimer);
-      return canvas.hud.tokenNoteHover.clear();
     }
   } else {
     return canvas.hud.tokenNoteHover.clear();
   }
+  return canvas.hud.tokenNoteHover.clear();
 });
