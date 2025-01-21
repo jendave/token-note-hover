0e70bf47043efab0864ed13bd347ac79edda6959
Syrious <syrious@gmx.net>
2025-01-21 19:27:10 +0100

    Fixes issue with npc notes

--- src/scripts/systems/a5e.js
+++ src/scripts/systems/a5e.js
@@ -1,45 +1,38 @@
-import { getTextFromNote } from "../textUtil.js";
+import { getTextFromNote, processNotes } from "../textUtil.js";
 
 export async function a5e(actor, displayImages) {
-  // Using a guard here looks cleaner
-  if (!actor) {
-    return null;
-  }
+    // Using a guard here looks cleaner
+    if (!actor) {
+        return null;
+    }
 
-  const actorIsOwner = actor.isOwner ?? true;
+    const actorIsOwner = actor.isOwner ?? true;
 
-  switch (actor.type) {
-    case "character":
-      return await getCharacterNotes(displayImages, actor, actorIsOwner);
-    case "npc":
-      return await getNpcNotes(displayImages, actor, actorIsOwner);
-    default:
-      return null;
-  }
+    switch (actor.type) {
+        case "character":
+            return await getCharacterNotes(displayImages, actor, actorIsOwner);
+        case "npc":
+            return await getNpcNotes(displayImages, actor, actorIsOwner);
+        default:
+            return null;
+    }
 }
 
 async function getCharacterNotes(displayImages, actor, actorIsOwner) {
-  return await processNotes(actor.system?.details?.notes, actorIsOwner, displayImages);
+    return await processNotes(actor.system?.details?.notes, actorIsOwner, displayImages);
 }
 
 async function getNpcNotes(displayImages, actor, actorIsOwner) {
-  let publicNotes = actor.system?.details?.notes;
-  let privateNotes = "";
+    const publicNotes = actor.system?.details?.notes;
+    const privateNotes = actor.system?.details?.privateNotes;
+    let notes = publicNotes;
 
-  if (game.user.isGM) {
-    privateNotes += "<br><h3>GM Notes</h3>";
-    privateNotes += actor.system?.details?.privateNotes;
-  }
+    if (game.user.isGM && privateNotes) {
+        notes += "<br><h3>GM Notes</h3>";
+        notes += privateNotes;
+    }
 
-  return await processNotes(publicNotes + privateNotes, actorIsOwner, displayImages);
+    return await processNotes(notes, actorIsOwner, displayImages);
 }
 
-async function processNotes(notes, actorIsOwner, displayImages) {
-  const renderedNotes = await getTextFromNote(notes, actorIsOwner);
 
-  if (!renderedNotes) {
-    return null;
-  }
-
-  return displayImages ? renderedNotes : renderedNotes.replaceAll(/<img.*>/g, "");
-}
