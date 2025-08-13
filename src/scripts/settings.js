153149fea9daaa1bf7954b8a0272ec82cd1b45b8
David Hudson <david.hudson@disney.com>
2025-08-12 19:52:04 -0700

    update to better defaults

--- src/scripts/settings.js
+++ src/scripts/settings.js
@@ -113,7 +113,7 @@ export default function registerSettings() {
     type: Number,
     default: 200,
     config: true,
-    range: { min: 100, max: 3000, step: 100 },
+    range: { min: 200, max: 3000, step: 100 },
   });
 
   game.settings.register(CONSTANTS.MODULE_ID, 'tooltipCloseDelay', {
@@ -121,9 +121,9 @@ export default function registerSettings() {
     hint: `${CONSTANTS.MODULE_ID}.Settings.TooltipCloseDelay.Hint`,
     scope: 'client',
     type: Number,
-    default: 300,
+    default: 100,
     config: true,
-    range: { min: 100, max: 3000, step: 100 },
+    range: { min: 0, max: 3000, step: 100 },
   });
 
   game.settings.register(CONSTANTS.MODULE_ID, 'tooltipColor', {
