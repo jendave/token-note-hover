c638d6bffe7c12510f97434fa65225a6465c3b17
David Hudson <david.hudson@disney.com>
2025-10-23 14:54:33 -0700

    add screenshot

--- src/scripts/systems/cairn.js
+++ src/scripts/systems/cairn.js
@@ -1,46 +1,48 @@
-import CONSTANTS from '../constants';
+import CONSTANTS from "../constants";
 import { processNotes } from "../textUtil.js";
 
-async function cairn(actor, displayImages) {
-  if (!actor) {
-    return null;
-  }
-  const actorIsOwner = actor.isOwner ?? true;
-  switch (actor.type) {
-    case "character":
-      if (game.settings.get(CONSTANTS.MODULE_ID, "displayPC")) {
-        return await getCharacterNotes(displayImages, actor, actorIsOwner);
-      } else {
+export async function cairn(actor, displayImages) {
+    // Using a guard here looks cleaner
+    if (!actor) {
         return null;
-      }
-    case "npc":
-      if (game.settings.get(CONSTANTS.MODULE_ID, "displayNPC")) {
-        return await getCharacterNotes(displayImages, actor, actorIsOwner);
-      } else {
-        return null;
-      }
-    default:
-      return null;
-  }
+    }
+    const actorIsOwner = actor.isOwner ?? true;
+
+    switch (actor.type) {
+        case "character":
+            if (game.settings.get(CONSTANTS.MODULE_ID, "displayPC")) {
+                return await getCharacterNotes(displayImages, actor, actorIsOwner);
+            } else {
+                return null;
+            }
+        case "npc":
+            if (game.settings.get(CONSTANTS.MODULE_ID, "displayNPC")) {
+                return await getCharacterNotes(displayImages, actor, actorIsOwner);
+            } else {
+                return null;
+            }
+        default:
+            return null;
+    }
 }
+
 async function getCharacterNotes(displayImages, actor, actorIsOwner) {
-  const Background = game.i18n.localize("CAIRN.Background");
-  const Role = game.i18n.localize("CAIRN.Role");
-  const Description = game.i18n.localize("CAIRN.Description");
-  let content = "";
-  if ((actor.system.background != null) && (actor.system.background != "")) {
-    content = content.concat(actor.type == 'npc' ? `<p><b>${Role}:</b>` : `<p><b>${Background}:</b>`);
-    content = content.concat(` ${actor.system.background}</p>`);
-  }
-  if ((actor.system.biography != null) && (actor.system.biography != "")) {
-    content = content.concat(`<b>${Description}:</b><br><p>${actor.system.biography}</p>`);
-  }
-  else if ((actor.system.description != null) && (actor.system.description != "")) {
-    content = content.concat(`<b>${Description}:</b><br>${actor.system.description}`);
-  }
-  if ((actor.system.notes != null) && (actor.system.notes != "")) {
-    const Notes = game.i18n.localize("CAIRN.Notes");
-    content = content.concat(`<b>${Notes}:</b>${actor.system.notes}`);
-  }
-  return await processNotes(content, actorIsOwner, displayImages);
+    const background = game.i18n.localize("CAIRN.Background");
+    const role = game.i18n.localize("CAIRN.Role");
+    const description = game.i18n.localize("CAIRN.Description");
+    let content = "";
+    if (actor.system.background != null && actor.system.background != "") {
+        content = content.concat(actor.type == "npc" ? `<p><b>${role}:</b>` : `<p><b>${background}:</b>`);
+        content = content.concat(` ${actor.system.background}</p>`);
+    }
+    if (actor.system.biography != null && actor.system.biography != "") {
+        content = content.concat(`<b>${description}:</b><br><p>${actor.system.biography}</p>`);
+    } else if (actor.system.description != null && actor.system.description != "") {
+        content = content.concat(`<b>${description}:</b><br>${actor.system.description}`);
+    }
+    if (actor.system.notes != null && actor.system.notes != "") {
+        const Notes = game.i18n.localize("CAIRN.Notes");
+        content = content.concat(`<b>${Notes}:</b>${actor.system.notes}`);
+    }
+    return await processNotes(content, actorIsOwner, displayImages);
 }
